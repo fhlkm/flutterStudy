37f1b5bdef8596e3f6692bfcc7d8feeda3f2abb7
hanlu.feng <fenghanlu@gmail.com>
2020-06-24 15:27:17 -0700

    statefulWidget的生命的生命周期

--- flutter_app/lib/pages/choose_location.dart
+++ flutter_app/lib/pages/choose_location.dart
@@ -3,10 +3,22 @@ class ChooseLocation extends StatefulWidget {
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
-
+//有状态Widget可以根据setState转变生命周期
+// init(only once)->build (setState)构建widget->Dispose()
+//
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter =0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("initState function run");
+  }
   @override
   Widget build(BuildContext context) {
+    print("build function run");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -15,6 +27,21 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
+      body: RaisedButton(
+        onPressed: (){
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text("counter is $counter"),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("dispose function run");
   }
 }

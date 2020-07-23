55d9b983bb43f26562fa2c9f838c0717dded2ce1
hanlu.feng <fenghanlu@gmail.com>
2020-07-23 10:26:08 -0700

    Example about Aysnc &await

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -5,6 +5,7 @@ import 'pages/choose_location.dart';
 
 
 void main() {
+  
 //  runApp(MyApp());
   runApp(MaterialApp(
 //  home: Text("Hanlu"),
@@ -24,4 +25,43 @@ void main() {
 
 
 
+List<int>_t1 = List<int>();
+justWait({@required int numberOfSeconds}) async {
+  await Future.delayed(Duration(seconds: numberOfSeconds));
+}
 
+test2() async {
+  await justWait(numberOfSeconds: 3);
+  _t1.clear();
+  print("t2: clear");
+}
+
+test1() async {
+  for (var k in _t1) {
+    print("t1: $k");
+    await justWait(numberOfSeconds: 1);
+  }
+  print("t1: clear");
+  _t1.clear();
+}
+
+/****************Async has problem*************************/
+//test() {
+//  print('=============================>');
+//  for (var i = 0; i < 10; i++ ) {
+//    _t1.add(i);
+//  }
+//  test1();
+//  test2();
+//  Future.delayed(Duration(seconds: 15), ()=> print(_t1));
+//}
+/****************Async fix problem*************************/
+//test() async {
+//  print('=============================>');
+//  for (var i = 0; i < 10; i++ ) {
+//    _t1.add(i);
+//  }
+//   await test1();
+//   test2();
+//  Future.delayed(Duration(seconds: 15), ()=> print(_t1));
+//}

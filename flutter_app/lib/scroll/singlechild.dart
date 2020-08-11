88e8fee350aa8592d32517935666af0f3cd6d52e
hanlu.feng <fenghanlu@gmail.com>
2020-08-11 10:45:34 -0700

    ClipRRect,ClipOval来剪切图片 SingleChild来处理超出屏幕不多 ListView.Builder相当于RecyclerView ListTitle需要一个Material ancestor Scaffold提供了一个路由的框架

---
+++ flutter_app/lib/scroll/singlechild.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Reading and Writing Files',
+      home:SingleChildScrollViewTestRoute(),
+    ),
+  );
+}
+
+//SingleChildScrollView只应在期望的内容不会超过屏幕太多时使用，这是因为SingleChildScrollView不支持基于Sliver的延迟实例化模型
+class SingleChildScrollViewTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return Scrollbar( // 显示进度条
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            //动态创建一个List<Widget>
+            children: str.split("")
+            //每一个字母都用一个Text显示,字体为原来的两倍
+                .map((c) => Text(c, textScaleFactor: 2.0,))
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}

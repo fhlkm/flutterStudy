e71cd3ea25ae94d2f91973bf9cff11dc15cfb457
hanlu.feng <fenghanlu@gmail.com>
2020-08-17 10:25:59 -0700

    1 依靠  数据变化-》build 来实现UI变化 2不依靠build来实现数据变化

--- flutter_app/lib/function/inherited.dart
+++ flutter_app/lib/function/inherited.dart
@@ -77,7 +77,7 @@ class ShareDataWidget extends InheritedWidget {
 
   //定义一个便捷方法，方便子树中的widget获取共享数据
   static ShareDataWidget of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();
+    return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();// trigger "didChangeDependencies"
   }
 
   //该回调决定当data发生变化时，是否通知子树中依赖data的Widget
